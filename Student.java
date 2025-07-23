b25d511a741fd21c098ee3f61609cd435f1bf35e
rahimathulla08 <168528448+rahimathulla08@users.noreply.github.com>
2025-07-23 13:09:02 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,12 @@
+public class StudentDetails {
+public static void main(String[] args) {
+String name = "Guru";
+String rollNo = "245122748059";
+String department = "Computer Science";
+System.out.println("Student Name: " + name);
+System.out.println("Roll No: " + rollNo);
+System.out.println("Department: " + department);
+}
+}
+
+
